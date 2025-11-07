a93508af2ac290e29316bdc2b68a067720d9ab67
Fernando Palacios <deliopalaciosviafara@gmail.com>
2025-11-06 20:06:07 -0500

    Implement CRUD operations for CategoriaHandler with error handling

--- handlers/categoria_handler.go
+++ handlers/categoria_handler.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"net/http"
+	"sistema_pos_go/models"
 	"sistema_pos_go/services"
+	"sistema_pos_go/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,4 +24,95 @@ func (h *CategoriaHandler) GetAll(c *gin.Context) {
 		return
 	}
 	c.IndentedJSON(http.StatusOK, categorias)
+}
+
+func (h *CategoriaHandler) GetById(c *gin.Context) {
+	id, err := utils.GetIDParam(c)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidID})
+		return
+	}
+
+	categoria, err := h.service.GetById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, categoria)
+}
+
+func (h *CategoriaHandler) Create(c *gin.Context) {
+	var categoria models.Categoria
+	if err := c.BindJSON(&categoria); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidData})
+		return
+	}
+
+	if err := h.service.Create(&categoria); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, categoria)
+}
+
+func (h *CategoriaHandler) Update(c *gin.Context) {
+	id, err := utils.GetIDParam(c)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidID})
+		return
+	}
+
+	var categoria models.Categoria
+	if err := c.BindJSON(&categoria); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidData})
+		return
+	}
+
+	if err := h.service.Update(id, &categoria); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, categoria)
+}
+
+/*
+func (h *CategoriaHandler) PartialUpdate(c *gin.Context) {
+	id, err := utils.GetIDParam(c)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidID})
+		return
+	}
+
+	var campos map[string]interface{}
+	if err := c.BindJSON(&campos); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidData})
+		return
+	}
+
+	categoria, err := h.service.PartialUpdate(id, campos)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, categoria)
+}
+*/
+
+func (h *CategoriaHandler) Delete(c *gin.Context) {
+	id, err := utils.GetIDParam(c)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidID})
+		return
+	}
+
+	if err := h.service.Delete(id); err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{"mensaje": utils.MsgDeleted})
 }
