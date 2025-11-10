6a33e8d5b2944daf50927ac6f98cbb5dd656a720
Fernando Palacios <deliopalaciosviafara@gmail.com>
2025-11-09 21:46:33 -0500

    reorganize validation constants and remove unused HTTP messages

--- utils/constants.go
+++ utils/constants.go
@@ -3,14 +3,14 @@ package utils
 // Constantes de validación
 const (
 	MaxNombreCategoriaLength = 100
+	MinId                    = 1
+	MinNumeroMesa            = 1
+	MinCapacidadMesa         = 1
 )
 
 // Constantes de mensajes HTTP
 const (
 	MsgInvalidID        = "ID inválido"
 	MsgInvalidData      = "Datos inválidos"
-	MsgNotFound         = "Recurso no encontrado"
-	MsgDeleted          = "Recurso eliminado correctamente"
-	MsgUpdated          = "Recurso actualizado correctamente"
 	MsgNoFieldsToUpdate = "No se enviaron campos para actualizar"
 )
