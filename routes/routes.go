064695fd6fc10fe6162eb800a57ecfbc2913660f
Fernando Palacios <deliopalaciosviafara@gmail.com>
2025-11-11 19:22:52 -0500

    Add middleware for error handling and recovery; initialize empleado routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"sistema_pos_go/handlers"
+	"sistema_pos_go/middleware"
 	"sistema_pos_go/repositories"
 	"sistema_pos_go/services"
 
@@ -13,17 +14,27 @@ import (
 func SetupRoutes(db *gorm.DB) *gin.Engine {
 	router := gin.Default()
 
+	// Middlewares globales
+	router.Use(middleware.RecoveryHandler())
+	router.Use(middleware.ErrorHandler())
+
+	// Handler para rutas no encontradas (404)
+	router.NoRoute(middleware.NoRouteHandler)
+
 	//Inicializar repos
 	categoriaRepo := repositories.NewCategoriaRepository(db)
 	mesaRepo := repositories.NewMesaRepository(db)
+	empleadoRepo := repositories.NewEmpleadoRepository(db)
 
 	//Inicializar servicios
 	categoriaService := services.NewCategoriaService(categoriaRepo)
 	mesaService := services.NewMesaService(mesaRepo)
+	empleadoService := services.NewEmpleadoService(empleadoRepo)
 
 	//Inicilizar hanlders
 	categoriaHandler := handlers.NewCategoriaHandler(categoriaService)
 	mesaHandler := handlers.NewMesaHandler(mesaService)
+	empleadoHandler := handlers.NewEmpleadoHandler(empleadoService)
 
 	//Grupo de rutas
 	api := router.Group("/api")
@@ -46,6 +57,16 @@ func SetupRoutes(db *gorm.DB) *gin.Engine {
 			mesas.PUT("/:id", mesaHandler.Update)
 			mesas.PATCH("/:id", mesaHandler.PartialUpdate)
 			mesas.DELETE("/:id", mesaHandler.Delete)
+		}
+		//Rutas empleado
+		empleados := api.Group("/empleados")
+		{
+			empleados.GET("", empleadoHandler.GetAll)
+			empleados.GET("/:id", empleadoHandler.GetById)
+			empleados.POST("", empleadoHandler.Create)
+			empleados.PUT("/:id", empleadoHandler.Update)
+			empleados.PATCH("/:id", empleadoHandler.PartialUpdate)
+			empleados.DELETE("/:id", empleadoHandler.Delete)
 		}
 
 	}
