3e30ab8c1f0fc9f782c5726e1374cdb9044348fd
Fernando Palacios <deliopalaciosviafara@gmail.com>
2025-11-11 19:23:43 -0500

    Refactor response handling in CategoriaHandler to use utility functions for error and success responses

--- handlers/categoria_handler.go
+++ handlers/categoria_handler.go
@@ -20,80 +20,75 @@ func NewCategoriaHandler(service *services.CategoriaService) *CategoriaHandler {
 func (h *CategoriaHandler) GetAll(c *gin.Context) {
 	categorias, err := h.service.GetAll()
 	if err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		utils.RespondWithError(c, http.StatusInternalServerError, err.Error())
 		return
 	}
-	c.IndentedJSON(http.StatusOK, categorias)
+	c.JSON(http.StatusOK, categorias)
 }
 
 func (h *CategoriaHandler) GetById(c *gin.Context) {
 	id, err := utils.GetIDParam(c)
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidID})
+		utils.RespondWithError(c, http.StatusBadRequest, utils.MsgInvalidID)
 		return
 	}
 
 	categoria, err := h.service.GetById(id)
 	if err != nil {
-		status := utils.GetHTTPStatusFromError(err)
-		c.IndentedJSON(status, gin.H{"error": err.Error()})
+		utils.RespondWithServiceError(c, err)
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, categoria)
+	c.JSON(http.StatusOK, categoria)
 }
 
 func (h *CategoriaHandler) Create(c *gin.Context) {
 	var categoria models.Categoria
-	if err := c.BindJSON(&categoria); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidData})
+	if err := c.ShouldBindJSON(&categoria); err != nil {
+		utils.RespondWithValidationError(c, err)
 		return
 	}
 
 	if err := h.service.Create(&categoria); err != nil {
-		status := utils.GetHTTPStatusFromError(err)
-		c.IndentedJSON(status, gin.H{"error": err.Error()})
+		utils.RespondWithServiceError(c, err)
 		return
 	}
 
-	c.IndentedJSON(http.StatusCreated, categoria)
+	c.JSON(http.StatusCreated, categoria)
 }
 
 func (h *CategoriaHandler) Update(c *gin.Context) {
 	id, err := utils.GetIDParam(c)
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidID})
+		utils.RespondWithError(c, http.StatusBadRequest, utils.MsgInvalidID)
 		return
 	}
 
 	var categoria models.Categoria
-	if err := c.BindJSON(&categoria); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidData})
+	if err := c.ShouldBindJSON(&categoria); err != nil {
+		utils.RespondWithValidationError(c, err)
 		return
 	}
 
 	if err := h.service.Update(id, &categoria); err != nil {
-		status := utils.GetHTTPStatusFromError(err)
-		c.IndentedJSON(status, gin.H{"error": err.Error()})
+		utils.RespondWithServiceError(c, err)
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, categoria)
+	c.JSON(http.StatusOK, categoria)
 }
 
 func (h *CategoriaHandler) Delete(c *gin.Context) {
 	id, err := utils.GetIDParam(c)
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": utils.MsgInvalidID})
+		utils.RespondWithError(c, http.StatusBadRequest, utils.MsgInvalidID)
 		return
 	}
 
 	if err := h.service.Delete(id); err != nil {
-		status := utils.GetHTTPStatusFromError(err)
-		c.IndentedJSON(status, gin.H{"error": err.Error()})
+		utils.RespondWithServiceError(c, err)
 		return
 	}
 
 	c.Status(http.StatusNoContent)
 }
-
